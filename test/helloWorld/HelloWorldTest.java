fb0a14fd4419c810c66e18608c0db20a9ae472b6
joshdickson40 <josh.dickson@wpi.edu>
2014-02-03 14:00:28 -0500

    Added a test comment
    Added a test to trigger a commit and a Travis build

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -1,12 +1,16 @@
 package helloWorld;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import uk.co.placona.helloWorld.HelloWorld;
 
 public class HelloWorldTest {
+	
+	/**
+	 * A test to run
+	 */
 	@Test
 	public void testHellowWorld(){
 		HelloWorld hello = new HelloWorld();
